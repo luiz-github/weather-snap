23f1f2e279cf401cbc48cb5858700c2a38a43bd1
Luiz Gustavo <luizgustavolimadrilard@gmail.com>
2025-05-26 20:33:40 -0300

    REFACTOR: getWeatherIcon removed

--- src/api/weather-api.tsx
+++ src/api/weather-api.tsx
@@ -1,26 +1,28 @@
 export class WeatherApi {
     private apiKey: string;
-    private baseUrl: string;
+    private baseWeatherURL: string;
+    private baseForecastURL: string;
 
     constructor() {
         this.apiKey = "fadb73083dda3bffc1adcd2ab6a4719d";
-        this.baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+        this.baseWeatherURL = "https://api.openweathermap.org/data/2.5/weather";
+        this.baseForecastURL = "https://api.openweathermap.org/data/2.5/forecast";
     }
 
+    //get current weather
     async getWeather(city: string): Promise<any> {
-        const response = await fetch(`${this.baseUrl}?q=${city}&units=imperial&appid=${this.apiKey}`);
-        
-        const url = `${this.baseUrl}?q=${city}&units=imperial&appid=${this.apiKey}`;
-        console.log(url)
+        const response = await fetch(`${ this.baseWeatherURL }?q=${city}&units=imperial&appid=${ this.apiKey }`);
+
+        const data = await response.json();
+        return data;
+    }
+
+    // get next five days forecast
+    async getForecast(city: string): Promise<any> {
+        const response = await fetch(`${ this.baseForecastURL }?q=${ city }&units=imperial&appid=${ this.apiKey }`);
 
         const data = await response.json();
         console.log(data); // console prints the API consult response
         return data;
-    }
-
-    getWeatherIcon(iconCode: string): string {
-        const response = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-        console.log(response);
-        return response;
     }
 }
